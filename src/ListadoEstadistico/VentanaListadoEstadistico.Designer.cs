95b5666738b7ae3bc07abb11ec446c2b49695d90
Shumaister <gabrielmaiori@gmail.com>
2018-06-17 21:54:49 -0300

    1- Cambiar estetica de top 5

--- src/ListadoEstadistico/VentanaListadoEstadistico.Designer.cs
+++ src/ListadoEstadistico/VentanaListadoEstadistico.Designer.cs
@@ -52,7 +52,7 @@
             // 
             // groupBox1
             // 
-            this.groupBox1.BackColor = System.Drawing.SystemColors.InactiveBorder;
+            this.groupBox1.BackColor = System.Drawing.SystemColors.ControlLightLight;
             this.groupBox1.Controls.Add(this.label8);
             this.groupBox1.Controls.Add(this.añoNUD);
             this.groupBox1.Controls.Add(this.label15);
@@ -62,9 +62,9 @@
             this.groupBox1.Controls.Add(this.label16);
             this.groupBox1.Controls.Add(this.cmbTipo);
             this.groupBox1.Controls.Add(this.label17);
-            this.groupBox1.Location = new System.Drawing.Point(0, 0);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(556, 254);
+            this.groupBox1.Size = new System.Drawing.Size(619, 382);
             this.groupBox1.TabIndex = 10;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "TOP 5";
@@ -72,15 +72,17 @@
             // label8
             // 
             this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(151, 8);
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label8.Location = new System.Drawing.Point(32, 40);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(275, 13);
+            this.label8.Size = new System.Drawing.Size(107, 20);
             this.label8.TabIndex = 18;
-            this.label8.Text = "A continuación, seleccione el top 5 que desea visualizar:";
+            this.label8.Text = "Estadisticas";
             // 
             // añoNUD
             // 
-            this.añoNUD.Location = new System.Drawing.Point(81, 85);
+            this.añoNUD.Location = new System.Drawing.Point(101, 174);
             this.añoNUD.Maximum = new decimal(new int[] {
             2025,
             0,
@@ -104,20 +106,26 @@
             // label15
             // 
             this.label15.AutoSize = true;
-            this.label15.Location = new System.Drawing.Point(8, 89);
+            this.label15.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label15.Location = new System.Drawing.Point(34, 181);
             this.label15.Name = "label15";
-            this.label15.Size = new System.Drawing.Size(29, 13);
+            this.label15.Size = new System.Drawing.Size(33, 13);
             this.label15.TabIndex = 16;
             this.label15.Text = "Año:";
             // 
             // btnConsultar
             // 
-            this.btnConsultar.Location = new System.Drawing.Point(424, 79);
+            this.btnConsultar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(61)))), ((int)(((byte)(101)))), ((int)(((byte)(129)))));
+            this.btnConsultar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnConsultar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnConsultar.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnConsultar.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnConsultar.Location = new System.Drawing.Point(476, 167);
             this.btnConsultar.Name = "btnConsultar";
-            this.btnConsultar.Size = new System.Drawing.Size(75, 23);
+            this.btnConsultar.Size = new System.Drawing.Size(108, 50);
             this.btnConsultar.TabIndex = 13;
             this.btnConsultar.Text = "Consultar";
-            this.btnConsultar.UseVisualStyleBackColor = true;
+            this.btnConsultar.UseVisualStyleBackColor = false;
             this.btnConsultar.Click += new System.EventHandler(this.btnConsultar_Click);
             // 
             // dataGridEstadisticas
@@ -129,10 +137,10 @@
             this.dataGridEstadisticas.BackgroundColor = System.Drawing.SystemColors.Control;
             this.dataGridEstadisticas.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridEstadisticas.GridColor = System.Drawing.SystemColors.Window;
-            this.dataGridEstadisticas.Location = new System.Drawing.Point(39, 119);
+            this.dataGridEstadisticas.Location = new System.Drawing.Point(35, 241);
             this.dataGridEstadisticas.Name = "dataGridEstadisticas";
             this.dataGridEstadisticas.ReadOnly = true;
-            this.dataGridEstadisticas.Size = new System.Drawing.Size(496, 115);
+            this.dataGridEstadisticas.Size = new System.Drawing.Size(549, 125);
             this.dataGridEstadisticas.TabIndex = 11;
             // 
             // cmbTrimestre
@@ -143,18 +151,19 @@
             "Abril-Junio",
             "Julio-Septiembre",
             "Octubre-Diciembre"});
-            this.cmbTrimestre.Location = new System.Drawing.Point(81, 56);
+            this.cmbTrimestre.Location = new System.Drawing.Point(101, 127);
             this.cmbTrimestre.Name = "cmbTrimestre";
-            this.cmbTrimestre.Size = new System.Drawing.Size(315, 21);
+            this.cmbTrimestre.Size = new System.Drawing.Size(164, 21);
             this.cmbTrimestre.TabIndex = 10;
             this.cmbTrimestre.Text = "Seleccione";
             // 
             // label16
             // 
             this.label16.AutoSize = true;
-            this.label16.Location = new System.Drawing.Point(6, 32);
+            this.label16.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label16.Location = new System.Drawing.Point(32, 87);
             this.label16.Name = "label16";
-            this.label16.Size = new System.Drawing.Size(31, 13);
+            this.label16.Size = new System.Drawing.Size(36, 13);
             this.label16.TabIndex = 9;
             this.label16.Text = "Tipo:";
             // 
@@ -167,18 +176,20 @@
             "Hoteles con mayor cantidad de días fuera de servicio.",
             "Habitaciones con mayor cantidad de días y veces que fueron ocupadas.",
             "Cliente con mayor cantidad de puntos."});
-            this.cmbTipo.Location = new System.Drawing.Point(81, 28);
+            this.cmbTipo.Location = new System.Drawing.Point(101, 84);
             this.cmbTipo.Name = "cmbTipo";
-            this.cmbTipo.Size = new System.Drawing.Size(315, 21);
+            this.cmbTipo.Size = new System.Drawing.Size(483, 21);
             this.cmbTipo.TabIndex = 7;
             this.cmbTipo.Text = "Seleccione...";
+            this.cmbTipo.SelectedIndexChanged += new System.EventHandler(this.cmbTipo_SelectedIndexChanged);
             // 
             // label17
             // 
             this.label17.AutoSize = true;
-            this.label17.Location = new System.Drawing.Point(6, 61);
+            this.label17.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label17.Location = new System.Drawing.Point(32, 135);
             this.label17.Name = "label17";
-            this.label17.Size = new System.Drawing.Size(53, 13);
+            this.label17.Size = new System.Drawing.Size(63, 13);
             this.label17.TabIndex = 6;
             this.label17.Text = "Trimestre:";
             // 
@@ -186,10 +197,10 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(554, 254);
+            this.ClientSize = new System.Drawing.Size(645, 406);
             this.Controls.Add(this.groupBox1);
             this.Name = "VentanaListadoEstadistico";
-            this.Text = "Form1";
+            this.Text = "Listados Estadisticos";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.Controls.SetChildIndex(this.logo, 0);
             this.Controls.SetChildIndex(this.groupBox1, 0);
--- src/ListadoEstadistico/VentanaListadoEstadistico.cs
+++ src/ListadoEstadistico/VentanaListadoEstadistico.cs
@@ -102,5 +102,10 @@ namespace FrbaHotel.ListadoEstadistico
                 }
             }
         }
+
+        private void cmbTipo_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
