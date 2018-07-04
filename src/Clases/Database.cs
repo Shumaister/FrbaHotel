74810ddcf3742b544dfcc646179c9817976553f1
dpoma <darioadrian96@gmail.com>
2018-07-04 01:48:02 -0300

    Yo me equivoque y pague, pero la expresividad no se mancha

--- src/Clases/Database.cs
+++ src/Clases/Database.cs
@@ -828,6 +828,7 @@ namespace FrbaHotel
             "JOIN RIP.Personas ON Cliente_PersonaID = Persona_ID " +
             "JOIN RIP.TiposDocumentos ON Persona_TipoDocumentoID = TipoDocumento_ID " +
             "JOIN RIP.Domicilios ON Persona_DomicilioID = Domicilio_ID WHERE Cliente_Estado = 1 AND " +
+            "Persona_DatoCorrupto = 0 AND " +
             "Persona_Nombre LIKE '" + cliente.persona.nombre + "%' AND " +
             "Persona_Apellido LIKE '" + cliente.persona.apellido + "%' AND " +
             "Persona_Email LIKE '" + cliente.persona.email + "%'" +
@@ -848,17 +849,7 @@ namespace FrbaHotel
             return bool.Parse(consultaObtenerValor(consulta));
         }
 
-        public static DataTable clienteObtenerTodosEnTabla()
-        {
-            SqlCommand consulta = consultaCrear("SELECT TOP 100 Cliente_ID, Cliente_Estado, Persona_ID, Persona_Nombre, Persona_Apellido, TipoDocumento_Descripcion, Persona_NumeroDocumento, Persona_Nacionalidad, Persona_FechaNacimiento, Persona_Telefono, Persona_Email, Domicilio_ID, Domicilio_Pais, Domicilio_Ciudad, Domicilio_Calle, Domicilio_NumeroCalle, Domicilio_Piso, Domicilio_Departamento FROM RIP.Clientes JOIN RIP.Personas ON Cliente_PersonaID = Persona_ID JOIN RIP.TiposDocumentos ON Persona_TipoDocumentoID = TipoDocumento_ID JOIN RIP.Domicilios ON Persona_DomicilioID = Domicilio_ID");
-            return consultaObtenerTabla(consulta);
-        }
 
-        public static DataTable clienteObtenerHabilitadosEnTabla()
-        {
-            SqlCommand consulta = consultaCrear("SELECT TOP 100 Cliente_ID, Persona_ID, Persona_Nombre, Persona_Apellido, TipoDocumento_Descripcion, Persona_NumeroDocumento, Persona_Nacionalidad, Persona_FechaNacimiento, Persona_Telefono, Persona_Email, Domicilio_ID, Domicilio_Pais, Domicilio_Ciudad, Domicilio_Calle, Domicilio_NumeroCalle, Domicilio_Piso, Domicilio_Departamento FROM RIP.Clientes JOIN RIP.Personas ON Cliente_PersonaID = Persona_ID JOIN RIP.TiposDocumentos ON Persona_TipoDocumentoID = TipoDocumento_ID JOIN RIP.Domicilios ON Persona_DomicilioID = Domicilio_ID WHERE Cliente_Estado = 1");
-            return consultaObtenerTabla(consulta);
-        }
 
         #endregion
 
